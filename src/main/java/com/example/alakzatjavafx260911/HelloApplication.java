318cb3a02b512e15cea149caff9ee015267f183b
Varga Ábel <abelvarga1848@gmail.com>
2026-09-14 10:56:50 +0200

    Start HelloApplication.java

--- src/main/java/com/example/alakzatjavafx260911/HelloApplication.java
+++ src/main/java/com/example/alakzatjavafx260911/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.alakzatjavafx260911;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Alakzatok");
         stage.setScene(scene);
+        Image windowIconImage = new Image("file:icons/icons/negyzet.png");
+        stage.getIcons().add(windowIconImage);
         stage.show();
     }
 
