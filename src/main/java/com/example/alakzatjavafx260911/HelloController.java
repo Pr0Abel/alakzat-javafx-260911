e90c30e018921d088068ef0215fc1e7163a20578
Varga Ábel <abelvarga1848@gmail.com>
2026-09-16 10:23:49 +0200

    Alakzat set on ImageView

--- src/main/java/com/example/alakzatjavafx260911/HelloController.java
+++ src/main/java/com/example/alakzatjavafx260911/HelloController.java
@@ -1,14 +1,78 @@
 package com.example.alakzatjavafx260911;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.text.Normalizer;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public RadioButton rb_piros;
+    @FXML
+    public RadioButton rb_zold;
+    @FXML
+    public RadioButton rb_kek;
+    @FXML
+    public RadioButton rb_kor;
+    @FXML
+    public RadioButton rb_haromszog;
+    @FXML
+    public RadioButton rb_negyzet;
+    @FXML
+    public ImageView iv_kep;
+    @FXML
+    public ListView lv_lista;
+    @FXML
+    public Label l_kep;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    String szin = "";
+    String alakzat = "";
+
+    public void initialize() {
+
+    }
+
+    public void onSzinClick(ActionEvent actionEvent) {
+        RadioButton radioButton = (RadioButton) actionEvent.getSource();
+        szin = radioButton.getText().toLowerCase();
+        l_kep.setStyle("-fx-background-color:red; -fx-border-color: black;");
+    }
+    public void onAlakzatClick(ActionEvent actionEvent) {
+        RadioButton radioButton = (RadioButton) actionEvent.getSource();
+        alakzat = radioButton.getText().toLowerCase();
+
+        String kepNev = switch (alakzat) {
+            case "háromszög" -> "haromszog.png";
+            case "kör" -> "kor.png";
+            case "négyzet" -> "negyzet.png";
+            default -> null;
+        };
+        System.out.println(kepNev);
+
+        if (kepNev != null) {
+            Image image = new Image("file:icons/icons/"+kepNev);
+
+            iv_kep.setImage(image);
+        }
+    }
+
+    public void onHozzaad(ActionEvent actionEvent) {
+
+    }
+
+    public void onTorol(ActionEvent actionEvent) {
+
+    }
+
+    public void onMentes(ActionEvent actionEvent) {
+
     }
 }
+
